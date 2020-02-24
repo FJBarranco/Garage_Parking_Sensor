14d3b25b460912773ffcb2cef7b7679e5af957bd
ferni305 <33739266+ferni305@users.noreply.github.com>
2020-02-24 16:08:21 -0500

    Initial commit

---
+++ Garage Park Assist/Parking_Garage_assist_v1_1/Park Assist/Park Assist/Debug/VM_DBG/VM_mem_check_sam.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\ferna\AppData\Local\Temp\VMBuilds\Park\ Assist\esp8266_generic\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2017\community\common7\ide\extensions\sivbitce.zrh\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
---
+++ Garage Park Assist/Parking_Garage_assist_v1_1/Parking_Garage_assist_v1_1.ino
@@ -0,0 +1,76 @@
+//Pins
+const int trigPin = 9;
+const int echoPin = 8;
+int relay1 = 11;
+int relay2 = 10;
+
+//Relay
+volatile byte relayState = LOW;
+
+// Variables
+long duration;
+int distance;
+
+void setup() {
+   Serial.begin(9600);
+   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+   for(int i = 1 ; i<=6;i++)
+{ 
+   pinMode(i,OUTPUT);
+}
+   pinMode(3, OUTPUT);
+   pinMode(relay1, OUTPUT);
+   digitalWrite(relay1, HIGH);
+   pinMode(relay2, OUTPUT);
+   digitalWrite(relay2, HIGH);
+  
+}
+void loop() {
+  // Clear trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Set trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Read echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+ 
+ if(distance <= 200)
+{
+  digitalWrite(relay2,LOW); // Turns ON Relay 1
+}
+
+delay(50);
+
+if (distance >=250)
+{
+  digitalWrite(relay2,HIGH); // Turns Relay Off 
+}
+
+  if(distance <= 3)
+{
+  digitalWrite(relay1,LOW); // Turns ON Relay 1
+}
+
+delay(50);
+
+if (distance >=10)
+{
+  digitalWrite(relay1,HIGH); // Turns Relay Off 
+}
+  //if (distance = 0)
+{
+  // Trigger fault alarm
+}
+}
